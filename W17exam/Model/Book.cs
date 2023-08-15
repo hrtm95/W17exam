9302af1ad8e064114a405d3534012cfab8d6f485
htrm95 <h.rostaminezhad1995@gmail.com>
2023-08-15 19:01:01 +0330

    add db context and models

---
+++ W17exam/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace W17exam.Model
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
---
+++ W17exam/Model/DataBaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace W17exam.Model
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("YourConnectionString");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Categories)
+                .WithMany(c => c.Books)
+                .UsingEntity(j => j.ToTable("BookCategories"));
+        }
+    }
+}
